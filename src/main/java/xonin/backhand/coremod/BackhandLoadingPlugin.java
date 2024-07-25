f17c6fd8690499126ed9d162660a6998f4f9a95b
Lyfts <127234178+Lyfts@users.noreply.github.com>
2024-07-25 21:54:17 +0200

    ditch this cursed lib once and for all

--- src/main/java/xonin/backhand/coremod/BackhandLoadingPlugin.java
+++ src/main/java/xonin/backhand/coremod/BackhandLoadingPlugin.java
@@ -2,26 +2,37 @@ package xonin.backhand.coremod;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-
-import net.tclproject.mysteriumlib.asm.common.CustomLoadingPlugin;
-import net.tclproject.mysteriumlib.asm.common.FirstClassTransformer;
 
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
 
 import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
-public final class BackhandLoadingPlugin extends CustomLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public final class BackhandLoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public String[] getASMTransformerClass() {
-        return new String[] { FirstClassTransformer.class.getName() };
+        return null;
     }
 
     @Override
-    public void registerFixes() {
-        registerClassWithFixes("net.tclproject.mysteriumlib.asm.fixes.MysteriumPatchesFixesO");
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {}
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
     }
 
     @Override
