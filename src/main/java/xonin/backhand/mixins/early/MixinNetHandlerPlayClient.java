c63624214a316deb5b0379a64db960d2d5e9ed0c
Lyft <127234178+Lyfts@users.noreply.github.com>
2024-07-27 00:11:59 +0200

    Update MixinNetHandlerPlayClient.java

--- src/main/java/xonin/backhand/mixins/early/MixinNetHandlerPlayClient.java
+++ src/main/java/xonin/backhand/mixins/early/MixinNetHandlerPlayClient.java
@@ -22,7 +22,7 @@ public abstract class MixinNetHandlerPlayClient {
             value = "INVOKE",
             target = "Lnet/minecraft/network/play/server/S09PacketHeldItemChange;func_149385_c()I",
             ordinal = 1))
-    private int backhand$isValidIventorySlot(int original) {
+    private int backhand$isValidInventorySlot(int original) {
         // return a valid int e.g. between 0 and < 9
         return InventoryPlayerBackhand.isValidSwitch(original) ? 0 : -1;
     }
