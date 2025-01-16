8b5b33ee53391badcf3135ec3b4317bd8db84c2c
Lyfts <127234178+Lyfts@users.noreply.github.com>
2025-01-16 22:41:23 +0100

    slight optimization

--- src/main/java/xonin/backhand/mixins/early/minecraft/MixinEntityOtherPlayerMP.java
+++ src/main/java/xonin/backhand/mixins/early/minecraft/MixinEntityOtherPlayerMP.java
@@ -5,11 +5,10 @@ import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.mojang.authlib.GameProfile;
 
@@ -19,31 +18,19 @@ import xonin.backhand.api.core.IBackhandPlayer;
 @Mixin(EntityOtherPlayerMP.class)
 public abstract class MixinEntityOtherPlayerMP extends AbstractClientPlayer implements IBackhandPlayer {
 
-    @Shadow
-    private boolean isItemInUse;
-
     private MixinEntityOtherPlayerMP(World p_i45074_1_, GameProfile p_i45074_2_) {
         super(p_i45074_1_, p_i45074_2_);
     }
 
-    @Inject(
+    @Redirect(
         method = "onUpdate",
-        cancellable = true,
         at = @At(
             value = "FIELD",
-            target = "Lnet/minecraft/client/entity/EntityOtherPlayerMP;isItemInUse:Z",
-            ordinal = 0))
-    private void backhand$isItemInUseHook(CallbackInfo ci) {
-        if (!isUsingOffhand()) return;
-        ItemStack offhand = BackhandUtils.getOffhandItem(this);
-        if (!isItemInUse && isEating() && offhand != null) {
-            setItemInUse(offhand, offhand.getMaxItemUseDuration());
-            isItemInUse = true;
-        } else if (isItemInUse && !isEating()) {
-            clearItemInUse();
-            isItemInUse = false;
-        }
-        ci.cancel();
+            opcode = Opcodes.GETFIELD,
+            args = "array=get",
+            target = "Lnet/minecraft/entity/player/InventoryPlayer;mainInventory:[Lnet/minecraft/item/ItemStack;"))
+    private ItemStack backhand$isItemInUseHook(ItemStack[] array, int index) {
+        if (isUsingOffhand()) return BackhandUtils.getOffhandItem(this);
+        return array[index];
     }
-
 }
