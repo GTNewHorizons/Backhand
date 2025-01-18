567a793bfde92fd6ea78e9dbf39ed41deaca1622
Lyfts <127234178+Lyfts@users.noreply.github.com>
2025-01-18 19:21:30 +0100

    fix pickblock placing block in wrong slot

--- src/main/java/xonin/backhand/mixins/early/minecraft/MixinMinecraft.java
+++ src/main/java/xonin/backhand/mixins/early/minecraft/MixinMinecraft.java
@@ -20,7 +20,9 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
@@ -209,6 +211,11 @@ public abstract class MixinMinecraft {
                 }
             });
         }
+    }
+
+    @ModifyConstant(method = "func_147112_ai", constant = @Constant(intValue = 9))
+    private int backhand$adjustSlotOffset(int constant) {
+        return 10;
     }
 
     @Unique
