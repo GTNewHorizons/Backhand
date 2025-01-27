4245ba09c52a5ebc498e5cfe974fcf61d8ec861e
Lyfts <127234178+Lyfts@users.noreply.github.com>
2025-01-27 20:00:19 +0100

    fix some item uis not opening when something is in offhand

---
+++ src/main/java/xonin/backhand/packet/OffhandCancelUsage.java
@@ -0,0 +1,28 @@
+package xonin.backhand.packet;
+
+import net.minecraft.client.Minecraft;
+
+import cpw.mods.fml.common.network.simpleimpl.IMessage;
+import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
+import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import io.netty.buffer.ByteBuf;
+
+public class OffhandCancelUsage implements IMessage {
+
+    public OffhandCancelUsage() {}
+
+    @Override
+    public void fromBytes(ByteBuf buf) {}
+
+    @Override
+    public void toBytes(ByteBuf buf) {}
+
+    public static class Handler implements IMessageHandler<OffhandCancelUsage, IMessage> {
+
+        @Override
+        public IMessage onMessage(OffhandCancelUsage message, MessageContext ctx) {
+            Minecraft.getMinecraft().thePlayer.clearItemInUse();
+            return null;
+        }
+    }
+}
