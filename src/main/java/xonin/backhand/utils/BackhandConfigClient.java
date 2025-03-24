cd3707d4097eb3bac18916cba759b95d69a743cf
Lyft <127234178+Lyfts@users.noreply.github.com>
2025-03-24 14:41:40 +0100

    Configurable offhand hotbar slot position (#79)

--- src/main/java/xonin/backhand/utils/BackhandConfigClient.java
+++ src/main/java/xonin/backhand/utils/BackhandConfigClient.java
@@ -11,4 +11,16 @@ public class BackhandConfigClient {
     @Config.Comment("If set to false, an empty offhand will only be rendered when the player is punching with the offhand.")
     @Config.DefaultBoolean(false)
     public static boolean RenderEmptyOffhandAtRest;
+
+    @Config.Comment("If set to false, the offhand hotbar slot will only be rendered when the offhand is not empty.")
+    @Config.DefaultBoolean(false)
+    public static boolean RenderOffhandHotbarSlotWhenEmpty;
+
+    @Config.RangeInt(min = -2000, max = 2000)
+    @Config.DefaultInt(0)
+    public static int offhandHotbarSlotXOffset;
+
+    @Config.RangeInt(min = 0, max = 1000)
+    @Config.DefaultInt(0)
+    public static int offhandHotbarSlotYOffset;
 }
