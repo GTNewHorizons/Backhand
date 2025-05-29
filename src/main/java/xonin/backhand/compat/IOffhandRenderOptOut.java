2f26130da2302716228835ccab94c67dd6802b80
YannickMG <yannickmg@gmail.com>
2025-05-28 21:14:44 -0400

    More IOffhandRenderOptOut JavaDoc

--- src/main/java/xonin/backhand/compat/IOffhandRenderOptOut.java
+++ src/main/java/xonin/backhand/compat/IOffhandRenderOptOut.java
@@ -1,7 +1,13 @@
 package xonin.backhand.compat;
 
+import jdk.jfr.Experimental;
+
 /**
  * Marker interface for custom renderers that want to override the default offhand rendering
+ * <br>
+ * Note: This is added mostly for expediency. It probably isn't required when rendering code is written especially for
+ * Backhand.
  */
+@Experimental
 public interface IOffhandRenderOptOut {
 }
