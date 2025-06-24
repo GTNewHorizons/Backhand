35cf69612c222a328fc26c91ade39dbe104125d9
Caedis <Caedis@users.noreply.github.com>
2025-06-24 17:05:56 -0500

    remove sword block config; no block if offhand

--- src/main/java/xonin/backhand/mixins/early/minecraft/MixinItemSword.java
+++ src/main/java/xonin/backhand/mixins/early/minecraft/MixinItemSword.java
@@ -1,33 +1,28 @@
 package xonin.backhand.mixins.early.minecraft;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
-import xonin.backhand.utils.BackhandConfig;
+import xonin.backhand.api.core.BackhandUtils;
 
 @Mixin(ItemSword.class)
 public abstract class MixinItemSword extends Item {
 
-    @ModifyReturnValue(method = "getItemUseAction", at = @At("RETURN"))
-    private EnumAction backhand$getItemUseAction(EnumAction original, ItemStack stack) {
-        return BackhandConfig.RemoveSwordBlock ? EnumAction.none : original;
-    }
-
-    @WrapWithCondition(
-        method = "onItemRightClick",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/entity/player/EntityPlayer;setItemInUse(Lnet/minecraft/item/ItemStack;I)V"))
-    private boolean backhand$onItemRightClick(EntityPlayer caller, ItemStack arg1, int arg2) {
-        return !BackhandConfig.RemoveSwordBlock;
+    @WrapMethod(method = "onItemRightClick")
+    private ItemStack backhand$onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer player,
+        Operation<ItemStack> original) {
+        if (BackhandUtils.getOffhandItem(player) != null) {
+            return itemStackIn;
+        } else {
+            return original.call(itemStackIn, worldIn, player);
+        }
     }
 }
