eb4fa1933d5db50c66f3d7defd61f73178b8e49f
sbancuz <sbancuz@protonmail.com>
2026-04-01 09:16:05 +0200

    Cancel only the needed events

--- src/main/java/xonin/backhand/compat/ExtraUtilsCompat.java
+++ src/main/java/xonin/backhand/compat/ExtraUtilsCompat.java
@@ -1,5 +1,6 @@
 package xonin.backhand.compat;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -9,7 +10,8 @@ import com.rwtema.extrautils.item.ItemHealingAxe;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import xonin.backhand.api.core.BackhandUtils;
+import ganymedes01.etfuturum.api.StrippedLogRegistry;
+import ganymedes01.etfuturum.api.mappings.RegistryMapping;
 import xonin.backhand.utils.Mods;
 
 @EventBusSubscriber
@@ -22,10 +24,19 @@ public class ExtraUtilsCompat {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) return;
         EntityPlayer player = event.entityPlayer;
-        ItemStack stack = BackhandUtils.getOffhandItem(player);
+
+        // Not using BackhandUtils.getOffhandItem since this event gets fired twice: once with the offhand, and another
+        // item with the mainhand. So we can just block one of the events
+        ItemStack stack = player.getHeldItem();
         if (stack != null && stack.getItem() instanceof ItemHealingAxe) {
-            event.setCanceled(true);
+            Block block = event.world.getBlock(event.x, event.y, event.z);
+            int meta = event.world.getBlockMetadata(event.x, event.y, event.z);
+            RegistryMapping<Block> mapping = StrippedLogRegistry.getLog(block, meta);
+            if (mapping != null) {
+                event.setCanceled(true);
+            }
         }
     }
 }
